6fc1d6046cb83c1c45220dab5fb70874619b9fef
Marakuyaspb <marakuyaspb@gmail.com>
2023-06-30 21:42:59 +0300

    Nav links are ok, articles pages are generated

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_articles)
+    path('', views.index_articles, name='articles'),
+    path('<int:pk>', views.ArticleFullView.as_view(),name='single_article'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Article
+from django.views.generic import DetailView
 
 # ARTICLES
 
 def index_articles(request):
-	#articles = Article.objects.all()	
-	#return render(request, 'main/index_articles.html', {'articless': article})
-	return render(request, 'main/index_articles.html', {})
+	articles = Article.objects.all()	
+	return render(request, 'main/index_articles.html', {'articles': articles})
+
+class ArticleFullView(DetailView):
+	model = Article
+	template_name = 'article/single_page.html'
+	context_object_name = 'article'
